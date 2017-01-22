baa22a70f81125c26b5779154a22a6440c27adfc
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-01-21 23:20:35 -0600

    Adds imports to generate_charts. Adds removelogs script for easy cleanup

---
+++ generate_charts.py
@@ -0,0 +1,9 @@
+import logging
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger()
+
+def generate_chart():
+    print("Called this function successfully")
+    logger.info("Generating chart")
+    logger.error("Generating chart - error!!")
