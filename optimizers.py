2d32df3a90189e9acf3be02858cee532fa178c3e
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-01-22 15:46:22 -0600

    Adds approx_func property to abstract class. This function is the approximate function that is updated with each iteration

--- optimizers.py
+++ optimizers.py
@@ -1,7 +1,7 @@
 import numpy as np
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 class Optimizer:
     """Optimizer class is a base class for various optimizers and
@@ -10,21 +10,23 @@ class Optimizer:
 
     def __init__(self,
                  func=None,
+                 approx_func=None,
                  gradient=None,
                  learning_rate=0.0001):
         """Must provide both a function and a gradient
         for evaluating y_hat as well as getting gradient
         values.
         """
+        self._true_coeff = func.get_parameters()
         self.function = func
+        self.approx_func = approx_func
         self.gradient = gradient
         self._learning_rate = learning_rate
-        self.weight_tracker = []
+        self.error_tracker = []
 
-        self.weights = np.array([])
         self._max_iter = 100000
 
-    def update_weights(self):
+    def update_weights(self, data):
         """Interface method - should be implemented in subclasses"""
         logger.error("Attempted to instantiate abstract Optimizer class")
         raise NotImplementedError("Must implement this method in inherited class")
@@ -48,10 +50,15 @@ class Optimizer:
         """
 
         old_weights = np.array([100 for _ in range(data[0, :].size)])
-
+        logger.info("Using mini-batch of size {}".format(data.shape[0]))
+        idx = 0
         for epoch_num in range(num_epochs):
 
             if idx > self._max_iter or np.allclose(old_weights, self.weights):
+                if idx > self._max_iter:
+                    logger.info("Stopping early due to exceeds max number of iterations")
+                else:
+                    logger.info("Stopping early due to slowed convergence")
                 break
 
             # Get batch
@@ -62,7 +69,7 @@ class Optimizer:
                                         axis=1)[batch_indices]
 
             # Run weight update
-            old_weights = self.weights
+            old_weights = self.approx_func.parameters
             self.update_weights(mini_batch)
 
 class SGD(Optimizer):
@@ -70,21 +77,22 @@ class SGD(Optimizer):
 
     def __init__(self,
                  func=None,
+                 approx_func=None
                  gradient=None,
                  learning_rate=0.0001):
-        super(SGD, self).__init__(func, gradient, learning_rate)
+        super(SGD, self).__init__(func, approx_func, gradient, learning_rate)
 
-    def update_weights(self, weights, data):
+    def update_weights(self, data):
         """Runs a single weight update and returns the updated weights"""
         # Need to make sure a gradient and func functions are passed in
 
-        x_vals = data[:,:-1]
+        x_vals = data[:, :-1]
         y = data[:, -1]
         aggregate_grad = 0
 
         for i in range(y.size):
             # Get diff between true y and estimated y
-            y_hat = self.function(x_vals[i, :], weights)
+            y_hat = self.approx_func.evaluate(x_vals[i, :])
             diff = y_hat - y[i]
 
             # Get gradient
@@ -92,7 +100,7 @@ class SGD(Optimizer):
 
         aggregate_grad /= y.size
 
-        weights -= self._learning_rate * aggregate_grad
+        self.approx_func.parameters -= self._learning_rate * aggregate_grad
 
 class ASSGD(Optimizer):
     """Accelerated Stochastic Gradient class - similar to SGD but uses
@@ -100,6 +108,7 @@ class ASSGD(Optimizer):
     """
     def __init__(self,
                  func=None,
+                 approx_func=None,
                  gradient=None,
                  threshold=0.01,
                  step_factor=0.5):
@@ -112,11 +121,11 @@ class ASSGD(Optimizer):
             func: function approximating the target function
             gradient: function that returns the gradient
         """
-        super(ASSGD, self).__init__(func, gradient)
+        super(ASSGD, self).__init__(func, approx_func, gradient)
         self._threshold = threshold
         self._step_factor = step_factor
 
-    def update_weights(self, weights, data):
+    def update_weights(self, data):
         """Runs a single weight update and returns the updated weights"""
 
         x_vals = data[:, :-1]
@@ -124,17 +133,73 @@ class ASSGD(Optimizer):
         aggregate_grad = 0
 
         for i in range(y.size):
-            y_hat = self.function(x_vals[i, :], weights)
+            y_hat = self.approx_func(x_vals[i, :])
             diff = y_hat - y[i]
 
             # Check diff and update learning rate if necessary
             if abs(diff) < self._threshold:
                 self._learning_rate *= self._step_factor
+                logger.info(("Difference in y_hat values below threshold - "
+                        "decreasing the step-size by a factor of {} to {}").format(
+                            self._learning_rate, self._step_factor))
 
             aggregate_grad += self.gradient(diff, x_vals[i, :])
 
         aggregate_grad /= y.size
 
-        weights -= self._learning_rate * aggregate_grad
+        self.approx_func.parameters -= self._learning_rate * aggregate_grad
 
-        return weights
+class SRGD(Optimizer):
+    """Stochastic Repeated Gradient Descent using adaptive step-size"""
+
+    def __init__(self,
+                 func=None,
+                 approx_func=None,
+                 gradient=None,
+                 threshold=0,
+                 step_factor=0.5,
+                 learning_rate=0.001,
+                 repeat_num=10):
+        """Initializer for SRGD. By default, adaptive step size is turned
+        off (threshold=0), thus by default SRGD uses a fixed step size
+
+        Parameters:
+
+            func: function being optimized
+            gradient: gradient function used by optimizer
+            threshold: threshold value used to adjust step size (default of 0
+                       means step size is fixed)
+            step_factor: scale factor used to geometrically decrease the step size
+            learning_rate: starting learning rate (step size), if threshold > 0,
+                           this value will geometrically decrease over time
+            repeat_num: number of iterations the weights are updated each time
+        """
+        super(SRGD, self).__init__(func, gradient, learning_rate)
+        self._threshold = threshold
+        self._step_factor = step_factor
+        self._repeat_num = repeat_num
+
+    def update_weights(self, data):
+        """ Runs SRGD update on the weight self.repeat_num of times"""
+
+        x_vals = data[:, :-1]
+        y = data[:, -1]
+        aggregate_grad = 0
+
+        for _ in range(self._repeat_num):
+            for i in range(y.size):
+                y_hat = self.approx_func(x_vals[i, :])
+                diff = y_hat - y[i]
+
+                # Check diff and update learning rate if necessary
+                if abs(diff) < self._threshold:
+                    self._learning_rate *= self._step_factor
+                    logger.info(("Difference in y_hat values below threshold - "
+                            "decreasing the step-size by a factor of {} to {}").format(
+                                self._learning_rate, self._step_factor))
+
+                aggregate_grad += self.gradient(diff, x_vals[i, :])
+
+            aggregate_grad /= y.size
+
+            self.approx_func.parameters -= aggregate_grad * self._learning_rate
