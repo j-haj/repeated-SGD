364112d528856118a3ef74ea86d632be4b7558ab
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-01-26 17:32:45 -0600

    Updates report and chart generation

--- generate_charts.py
+++ generate_charts.py
@@ -29,26 +29,32 @@ def load_data(filename):
             err1 = float(vals[6])
             err2 = float(vals[7])
             if r not in data[key]:
-                data[key][r] = {
-                        "batch_size": [],
+                data[key][r] = {batch_sz: {
+                        "n_iter": [],
+                        "time": [],
+                        "norm_err": [],
+                        "max_err": []}}
+            elif r in data[key] and batch_sz not in data[key][r]:
+                data[key][r][batch_sz] = {
                         "n_iter": [],
                         "time": [],
                         "norm_err": [],
                         "max_err": []}
 
-            data[key][r]["batch_size"].append(batch_sz)
-            data[key][r]["n_iter"].append(n_iter)
-            data[key][r]["time"].append(time)
-            data[key][r]["norm_err"].append(err1)
-            data[key][r]["max_err"].append(err2)
+            data[key][r][batch_sz]["n_iter"].append(n_iter)
+            data[key][r][batch_sz]["time"].append(time)
+            data[key][r][batch_sz]["norm_err"].append(err1)
+            data[key][r][batch_sz]["max_err"].append(err2)
     return data
 
-def generate_scatter_err_vs_time(results, plt_name):
+def generate_scatter_err_vs_time(results, batch_size, plt_name):
     """Generates two scatter plots showing error
     as a function of runtime for the various methods.
     """
     fig, ax = plt.subplots()
-    
+    print("Summary for {}".format(plt_name))
+    print("Batch size: {}".format(batch_size))
+
     # Set labels and title
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Norm Error")
@@ -77,33 +83,51 @@ def generate_scatter_err_vs_time(results, plt_name):
         for key in plt_data:
             plt_data[key][r] = {"x": 0, "y": {"norm": 0, "max": 0}}
             # Get x vals
-            plt_data[key][r]["x"] = results[key][r]["time"] 
+            plt_data[key][r]["x"] = results[key][r][batch_size]["time"]
             #sum(results[key][r]["time"])/len(results[key][r]["time"])
 
             # Get y vals
-            plt_data[key][r]["y"]["norm"] = results[key][r]["norm_err"]
+            plt_data[key][r]["y"]["norm"] = results[key][r][batch_size]["norm_err"]
             #sum(results[key][r]["norm_err"])/len(results[key][r]["norm_err"])
 
-            plt_data[key][r]["y"]["max"] = \
-            sum(results[key][r]["max_err"])/len(results[key][r]["max_err"])
+            plt_data[key][r]["y"]["max"] = results[key][r][batch_size]["max_err"]
 
     for key in plt_data:
         if "r" in key:
             # One of the repeat optimizers
             for r in r_vals:
                 label = key + " - " + str(r)
+                x_val = plt_data[key][r]["x"]
+                y_norm = plt_data[key][r]["y"]["norm"]
+                y_max = plt_data[key][r]["y"]["max"]
                 ax.scatter(plt_data[key][r]["x"],
                         plt_data[key][r]["y"]["norm"],
                         label=label,
                         marker=styles[label]["marker"],
                         color=styles[label]["color"])
+                
+                avg_time = sum(x_val)/len(x_val)
+                avg_norm = sum(y_norm)/len(y_norm)
+                avg_max = sum(y_max)/len(y_max)
+                print("{}\n\tavg time: {:.4f}\n\tavg norm: {:.4f}\n\tavg max: {:.4f}".format(
+                    label, avg_time, avg_norm, avg_max))
         else:
             # Non-repeat optimizes
+            x_val = plt_data[key][1]["x"]
+            y_norm = plt_data[key][1]["y"]["norm"]
+            y_max = plt_data[key][1]["y"]["max"]
             ax.scatter(plt_data[key][1]["x"],
                     plt_data[key][1]["y"]["norm"],
                     label=key,
                     marker=styles[key]["marker"],
                     color=styles[key]["color"])
+
+            avg_time = sum(x_val)/len(x_val)
+            avg_norm = sum(y_norm)/len(y_norm)
+            avg_max = sum(y_max)/len(y_max)
+            print("{}\n\tavg time: {:.4f}\n\tavg norm: {:.4f}\n\tavg max: {:.4f}".format(
+                key, avg_time, avg_norm, avg_max))
+
     ax.legend()
     ax.grid(True)
 
@@ -116,5 +140,11 @@ def generate_chart():
     logger.error("Generating chart - error!!")
 
 if __name__ == "__main__":
-    data = load_data("results_10_0001.csv")
-    generate_scatter_err_vs_time(data, "dim-10.png")
+    data_dim50 = load_data("results_50_0001.csv")
+    data_dim10 = load_data("results_10_0001.csv")
+    generate_scatter_err_vs_time(data_dim50, 1, "dim-50-batch-1.png")
+    generate_scatter_err_vs_time(data_dim50, 50, "dim-50-batch-50.png")
+    generate_scatter_err_vs_time(data_dim50, 500, "dim-50-batch-500.png")
+    generate_scatter_err_vs_time(data_dim10, 1, "dim-10-batch1.png")
+    generate_scatter_err_vs_time(data_dim10, 50, "dim-10-batch-50.png")
+    generate_scatter_err_vs_time(data_dim10, 500, "dim-10-batch-500.png")
